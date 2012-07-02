00e1f81697095f8bc6fce39e77c49b4f0094ef17
Ivan Zezyulya <ivan.zezyulya@aldigit.com>
2012-07-02 20:27:44 +0700

    Slightly change project settings;
    Change tabs to spaces

--- XCode4VirtualTabs.m
+++ XCode4VirtualTabs.m
@@ -10,95 +10,97 @@ typedef void (*selectorFunction)(id, SEL, SEL);
 @implementation XCode4VirtualTabs
 static void doCommandBySelector( id self_, SEL _cmd, SEL selector )
 {
-	do {
-		bool selectionModified = (selector == @selector(moveToBeginningOfLineAndModifySelection:)) ||
-			(selector == @selector(moveToLeftEndOfLineAndModifySelection:));
-		
-		if (selector == @selector(moveToBeginningOfLine:) ||
+    //NSLog(@"selector = %@", NSStringFromSelector(selector));
+
+    do {
+        bool selectionModified = (selector == @selector(moveToBeginningOfLineAndModifySelection:)) ||
+            (selector == @selector(moveToLeftEndOfLineAndModifySelection:));
+        
+        if (selector == @selector(moveToBeginningOfLine:) ||
             selector == @selector(moveToLeftEndOfLine:) || selectionModified)
-		{
-			NSTextView *self = (NSTextView *)self_;
-			NSString *text = self.string;
-			NSRange selectedRange = self.selectedRange;
-			NSRange lineRange = [text lineRangeForRange:selectedRange];
-			
-			if (lineRange.length == 0)
-				break;
-			
-			NSString *line = [text substringWithRange:lineRange];
-			NSRange codeStartRange = [line rangeOfCharacterFromSet:[[NSCharacterSet whitespaceCharacterSet] invertedSet]];
-			
-			if (codeStartRange.location == NSNotFound)
-				break;
-			
-			NSUInteger caretLocation = selectedRange.location - lineRange.location;
-			if (caretLocation < codeStartRange.location && caretLocation != 0)
-				break;
-			
-			int start = lineRange.location;
-			if (selectedRange.location != (lineRange.location + codeStartRange.location))
-				start += codeStartRange.location;
-			
-			int end = selectionModified ? (selectedRange.location + selectedRange.length) : start;
-			
-			if (end - start < 0)
-				break;
-			
+        {
+            NSTextView *self = (NSTextView *)self_;
+            NSString *text = self.string;
+            NSRange selectedRange = self.selectedRange;
+            NSRange lineRange = [text lineRangeForRange:selectedRange];
+            
+            if (lineRange.length == 0)
+                break;
+            
+            NSString *line = [text substringWithRange:lineRange];
+            NSRange codeStartRange = [line rangeOfCharacterFromSet:[[NSCharacterSet whitespaceCharacterSet] invertedSet]];
+            
+            if (codeStartRange.location == NSNotFound)
+                break;
+            
+            NSUInteger caretLocation = selectedRange.location - lineRange.location;
+            if (caretLocation < codeStartRange.location && caretLocation != 0)
+                break;
+            
+            int start = lineRange.location;
+            if (selectedRange.location != (lineRange.location + codeStartRange.location))
+                start += codeStartRange.location;
+            
+            int end = selectionModified ? (selectedRange.location + selectedRange.length) : start;
+            
+            if (end - start < 0)
+                break;
+            
             NSRange range = NSMakeRange(start, end - start);
-			
-			[self setSelectedRange:range];
-			[self scrollRangeToVisible:range];
-			return;
-		}
-		else if (selector == @selector(moveLeft:) ||
-				 selector == @selector(moveRight:))
-		{
-			NSTextView *self = (NSTextView *)self_;
-			NSString *text = self.string;
-			NSRange selectedRange = self.selectedRange;
-			NSRange lineRange = [text lineRangeForRange:selectedRange];
+            
+            [self setSelectedRange:range];
+            [self scrollRangeToVisible:range];
+            return;
+        }
+        else if (selector == @selector(moveLeft:) ||
+                 selector == @selector(moveRight:))
+        {
+            NSTextView *self = (NSTextView *)self_;
+            NSString *text = self.string;
+            NSRange selectedRange = self.selectedRange;
+            NSRange lineRange = [text lineRangeForRange:selectedRange];
 
-			NSString *line = [text substringWithRange:lineRange];
-			NSRange codeStartRange = [line rangeOfCharacterFromSet:[[NSCharacterSet whitespaceCharacterSet] invertedSet]];
+            NSString *line = [text substringWithRange:lineRange];
+            NSRange codeStartRange = [line rangeOfCharacterFromSet:[[NSCharacterSet whitespaceCharacterSet] invertedSet]];
 
-			if (codeStartRange.location == NSNotFound)
-				break;
+            if (codeStartRange.location == NSNotFound)
+                break;
 
-			NSUInteger caretLocation = selectedRange.location - lineRange.location;
-			
-			if (selector == @selector(moveLeft:)) {
-				if (caretLocation != 0 && caretLocation <= codeStartRange.location) {
-					char previousCharacter = [text characterAtIndex:selectedRange.location - 1];
-					if (previousCharacter == ' ') {
-						NSInteger newCaretlocation = ((caretLocation/4) - 1)*4;
-						if (newCaretlocation < 0) {
-							newCaretlocation = 0;
-						}
-						NSRange range = NSMakeRange(selectedRange.location + (newCaretlocation - caretLocation), 0);
-						[self setSelectedRange:range];
-						[self scrollRangeToVisible:range];
-						return;
-					}
-				}
-			}
-			else if (selector == @selector(moveRight:)) {
-				if (caretLocation < codeStartRange.location) {
-					char nextCharacter = [text characterAtIndex:selectedRange.location + 1];
-					if (nextCharacter == ' ') {
-						NSInteger newCaretlocation = ((caretLocation/4) + 1)*4;
-						if (newCaretlocation > codeStartRange.location) {
-							newCaretlocation = codeStartRange.location;
-						}
-						NSRange range = NSMakeRange(selectedRange.location + (newCaretlocation - caretLocation), 0);
-						[self setSelectedRange:range];
-						[self scrollRangeToVisible:range];
-						return;
-					}
-				}
-			}
-		}
-	} while (0);
-	
+            NSUInteger caretLocation = selectedRange.location - lineRange.location;
+            
+            if (selector == @selector(moveLeft:)) {
+                if (caretLocation != 0 && caretLocation <= codeStartRange.location) {
+                    char previousCharacter = [text characterAtIndex:selectedRange.location - 1];
+                    if (previousCharacter == ' ') {
+                        NSInteger newCaretlocation = ((caretLocation/4) - 1)*4;
+                        if (newCaretlocation < 0) {
+                            newCaretlocation = 0;
+                        }
+                        NSRange range = NSMakeRange(selectedRange.location + (newCaretlocation - caretLocation), 0);
+                        [self setSelectedRange:range];
+                        [self scrollRangeToVisible:range];
+                        return;
+                    }
+                }
+            }
+            else if (selector == @selector(moveRight:)) {
+                if (caretLocation < codeStartRange.location) {
+                    char nextCharacter = [text characterAtIndex:selectedRange.location + 1];
+                    if (nextCharacter == ' ') {
+                        NSInteger newCaretlocation = ((caretLocation/4) + 1)*4;
+                        if (newCaretlocation > codeStartRange.location) {
+                            newCaretlocation = codeStartRange.location;
+                        }
+                        NSRange range = NSMakeRange(selectedRange.location + (newCaretlocation - caretLocation), 0);
+                        [self setSelectedRange:range];
+                        [self scrollRangeToVisible:range];
+                        return;
+                    }
+                }
+            }
+        }
+    } while (0);
+    
     return ((selectorFunction)original_doCommandBySelector)(self_, _cmd, selector);
 }
 
